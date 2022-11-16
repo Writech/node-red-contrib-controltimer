2635b1fb9ae435ee49c908d79110eb50742baf63
Robert Vane <robertvane@gmail.com>
2022-11-16 16:46:05 +0200

    Update LICENSE.

--- scripts/clean/all.sh
+++ scripts/clean/all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ./scripts/_utils/_variables.sh
 
-npx rimraf dist tsconfig.tsbuildinfo package "$PACKAGE_JSON_NAME-*.tgz" node_modules package-lock.json
+npx rimraf dist tsconfig.tsbuildinfo package "$PACKAGE_JSON_NAME-*.tgz" node_modules package-lock.json .husky
