c5e65b36d8a91608cd86e411897f871cec1859c0
Robert Vane <robertvane@gmail.com>
2023-05-27 22:31:08 +0300

    Bump patch version in package.json. Restructure scripts/dev.sh script to multiple lines.

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-#npm run node-red:start-docker & nodemon --watch "./src" --exec "npm run node-red:reinstall-package"
-
-concurrently --names "NODE-RED,REINSTALL" --prefix-colors "bgRed,bgBlue" "npm run node-red:start-docker" "nodemon --watch src --ext ts,ejs,png --exec 'npm run node-red:reinstall-package'"
+concurrently \
+    --names "NODE-RED,REINSTALL" \
+    --prefix-colors "bgRed,bgBlue" \
+    "npm run node-red:start-docker" \
+    "nodemon --watch src --ext ts,ejs,png --exec 'npm run node-red:reinstall-package'"
